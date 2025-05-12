e4c53efd3c703859ddb55ff6bcb241e41b536e31
Nico Faerber <nico.faerber@unibe.ch>
2025-05-12 13:47:22 +0200

    Fix URL

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 [ -d gh-pages ] && rm -rf gh-pages
 
-git clone git@github.com:hpc-unibe-ch/hpc-unibe-ch.github.io.git gh-pages
+git clone git@github.com:nfaerber/nfaerber.github.io.git gh-pages
 cd gh-pages
 mkdocs gh-deploy --config-file ../mkdocs.yml --remote-branch main
 cd ..
